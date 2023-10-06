629dfc86f426888e6ed19c590bb072c9d51eab74
Siddhartha Rajeev <124267492+sidsr003@users.noreply.github.com>
2023-10-06 05:37:05 +0530

    Delete Handwritten Digit Classifier directory

--- Handwritten Digit Classifier/layers.py
+++
@@ -1,9 +0,0 @@
-class Layer():
-    def __init__(self):
-        self.input = None
-        self.output = None
-    def forwardPropagation(self, inputData):
-        raise NotImplementedError
-
-    def backwardPropagation(self, outputError, learningRate):
-        raise NotImplementedError
